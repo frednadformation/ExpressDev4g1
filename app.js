4fb85ab8becb4e8be10f4136dc536922c85adf18
Frederic Nadaradjane Formation <frederic.nadaradjane@lagrandeclasse.fr>
2024-01-04 19:53:10 +0100

    change corsv2

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ app.use(cors({credentials: true, origin: process.env.FRONTEND_URL}));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
   });
 
